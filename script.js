3fead97013e1731ef74773748de15b9058239f2a
CastorDisaster <alexmnevins@gmail.com>
2024-04-14 11:46:30 -0400

    add knight logic algorithm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. create a chessboard represented as a graph (2D array)
+// 1. create a chessboard represented as a graph (array)
 // (2d coordinates: spaces are represented as [x, y])
 
 class ChessBoard {
@@ -10,21 +10,52 @@ class ChessBoard {
         let output = [];
 
         for (let i = 0; i<=7; i++) {
-            let currentRow = [];
             for (let j = 0; j <= 7; j++) {
-                currentRow.push([j, i]);
+                output.push([j, i]);
             }
-            output.push(currentRow);
         }
         return output;
     }
 
-
-    
-
     // 2. create a adjacency matrix representing the possible moves of a knight on any space
 
+    generateAdjacencyList () {
+        let output = [];
 
+        this.board.forEach((square) => {
+            let current = [];
+            this.board.forEach((comparison) => {
+                
+            })
+                
+        })
+
+
+
+         
+
+    }
+
+    //helper function: returns boolean to check whether knight can move from origin to current
+    knightLogic(origin, current) {
+
+        if (
+            this.plusMinus(origin[0], current[0], 1) &&
+            this.plusMinus(origin[1], current[1], 2) || 
+            this.plusMinus(origin[0], current[0], 2) &&
+            this.plusMinus(origin[1], current[1], 1)
+        ) return true;
+
+        else return false;
+    }
+
+    //helper function : returns boolean to check whether y equals x, plus or minus 2
+    plusMinus(x, y, n) {
+        if (y === x + n || y === x - n) return true;
+        else return false;
+    }
+
+   
 
 
 }
@@ -49,4 +80,4 @@ const knightMoves = function (start, end) {
 // driver code
 
 const myBoard = new ChessBoard();
-console.log(myBoard.board);
+console.log(myBoard.knightLogic([3, 3], [4, 1]));
